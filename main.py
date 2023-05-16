d47b18fb6f4ab0efbfa363b236984bec7632d9de
linndfors <olha.nahurna@ucu.edu.ua>
2023-05-16 21:49:28 +0200

    fix bugs

--- main.py
+++ main.py
@@ -9,20 +9,10 @@ def find_sentence(path: str, n: int, app_flag = 0):
     """
     Find the most important n sentences
     """
-    r = 70
-    q = 5
-    p = 5
     data_frame, sentence_list = parse(path, app_flag)
     sentence_list = np.array(sentence_list)
     matrix_of_words = data_frame.values
-
-    start_time = time.time()
     U, S, Vt = reduced_svd(matrix_of_words.T)
-    # U, S, Vt = np.linalg.svd(matrix_of_words.T)
-    # U, S, Vt = r_svd(matrix_of_words.T, r, q, p)
-    finish_time = time.time()
-    print("Time: ", finish_time - start_time)
-
     scores_for_sentences = np.array(count_score(Vt, S))
     indices_of_scores = np.argsort(-scores_for_sentences)
     best_sentences = sentence_list[indices_of_scores][: n]
